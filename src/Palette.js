6425ebc51cf2773af5f3f8f2515612358738e6d3
Willian Welbert <willianwelbert@yahoo.com.br>
2019-06-19 17:11:25 -0300

    first commit, updates readme and adds palette

---
+++ src/Palette.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+  render() {
+    return (
+      <div>
+        <div className='Palette'>
+        {/*Navbar goes here*/}
+          <div className='Palette-colors'>
+           {/*bunch of color boxes*/}
+          </div>
+          {/*footer*/}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Palette;
