5a29a525e545a204afe25d5b25ac414a5ab35493
Willian Welbert <willianwelbert@yahoo.com.br>
2019-06-19 18:23:28 -0300

    adds clipboard dependency and sets it up

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import './ColorBox.css'
+import './ColorBox.css';
+
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 class ColorBox extends Component {
   render() {
     const{name, background} = this.props;
     return (
+      <CopyToClipboard text={background}>
       <div style={{background}} className='ColorBox'>
         <div className='copy-container'>
           <div className='box-content'>
@@ -14,6 +17,7 @@ class ColorBox extends Component {
         </div>
         <span className='see-more' >more</span>
       </div>
+      </CopyToClipboard>
     );
   }
 }
