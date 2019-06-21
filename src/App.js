952062bbfb16ca5cdcb6fb1daaa89c766fb2348c
Willian Welbert <willianwelbert@yahoo.com.br>
2019-06-21 15:50:15 -0300

    adds navbar

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import {generatePalette} from './colorHelpers';
 import Palette from './Palette';
 
 function App() {
-  console.log(generatePalette(seedColors[4]))
+  
   return (
     <div className="App"> 
-      <Palette {...seedColors[4]} />
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
